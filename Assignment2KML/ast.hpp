fc39d7823de95f0c6f00b71295cabfeb2f88ab98
Charles Bisbee <bisbeecharlesl@gmail.com>
2017-10-04 11:25:25 -0600

    Starting some of the .l and .y files

---
+++ Assignment2KML/ast.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <string>
+
+class Node {
+public: enum { KML, DOCUMENT, MAJOR_TAG, MINOR_TAG, NAME_TAG, DESCRIPTION_TAG, POINT_TAG, COORD_TAG, STRING_LITERAL, NUMBER_LITERAL};
+public: std::vector < std::shared_ptr < Node > > children;
+public: virtual int type() const = 0;
+public: virtual ~Node();
+};
+
+class StringNode : public Node {
+public: std::string strLiteral;
+public: StringNode(const std::string &_strLiteral);
+public: int type() const;
+};
+
+class NumberNode : public Node {
+public: int number;
+public: NumberNode(int _number);
+public: int type() const;
+};
+
+class StartCommandNode : public Node {
+public: StartCommandNode(std::shared_ptr< Node > word, std::shared_ptr < Node > number);
+public: int type() const;
+
+public: const std::string & target() const;
+
+public: int power() const;
+  
+};
+
+class StopCommandNode : public Node {
+public: StopCommandNode(std::shared_ptr< Node > word);
+public: int type() const;
+
+public: const std::string & target() const;
+
+};
+
+class KMLNode : public Node {
+  public: int type() const;
+};
+
+
+typedef std::shared_ptr < Node > NodePtr;
+typedef std::shared_ptr < ProgramNode > ProgramNodePtr;
+typedef std::shared_ptr < WordNode > WordNodePtr;
+typedef std::shared_ptr < NumberNode > NumberNodePtr;
+typedef std::shared_ptr < StartCommandNode > StartCommandNodePtr;
+typedef std::shared_ptr < StopCommandNode > StopCommandNodePtr;
+
+int number(const NodePtr p);
+
+const std::string & word(const NodePtr p);
+
+NodePtr node(int number);
+
+NodePtr node(const std::string &word);
