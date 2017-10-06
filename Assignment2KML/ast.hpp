c5a7a53208fd29dc5c262c74cd1e72d4e9532c58
Charles Bisbee <bisbeecharlesl@gmail.com>
2017-10-05 18:00:48 -0600

    working on building up the tree nodes

--- Assignment2KML/ast.hpp
+++ Assignment2KML/ast.hpp
@@ -3,60 +3,114 @@
 #include <vector>
 #include <memory>
 #include <string>
+#include <iostream>
 
 class Node {
-public: enum { KML, DOCUMENT, MAJOR_TAG, MINOR_TAG, NAME_TAG, DESCRIPTION_TAG, POINT_TAG, COORD_TAG, STRING_LITERAL, NUMBER_LITERAL};
+public: enum {
+    PROGRAM,
+    KML,
+    DOCUMENT,
+    PLACEMARKER,
+    NAME,
+    DESCRIPTION,
+    POINT,
+    COORDINATE,
+    NUMBER_LITERAL,
+    STRING_LITERAL,
+    COMMA_DELIMETER
+  };
 public: std::vector < std::shared_ptr < Node > > children;
 public: virtual int type() const = 0;
+public: virtual void print(std::ostream &out) const = 0;
 public: virtual ~Node();
 };
 
-class StringNode : public Node {
-public: std::string strLiteral;
-public: StringNode(const std::string &_strLiteral);
+typedef std::shared_ptr < Node > NodePtr;
+
+class StringLiteralNode : public Node {
+public: std::string stringLiteral;
+public: StringLiteralNode(const std::string &_stringLiteral);
 public: int type() const;
+public: void print(std::ostream &out) const;
 };
+typedef std::shared_ptr < StringLiteralNode > StringLiteralNodePtr;
 
-class NumberNode : public Node {
-public: int number;
-public: NumberNode(int _number);
+class NumberLiteralNode : public Node {
+public: int numberLiteral;
+public: NumberLiteralNode(int _numberLiteral);
 public: int type() const;
+public: void print(std::ostream &out) const;  
 };
+typedef std::shared_ptr < NumberLiteralNode > NumberLiteralNodePtr;
 
-class StartCommandNode : public Node {
-public: StartCommandNode(std::shared_ptr< Node > word, std::shared_ptr < Node > number);
+
+class CommaDelimeterNode : public Node {
+public: char commaDelimeter;
+public: CommaDelimeterNode(char _commaDelimeter);
 public: int type() const;
-
-public: const std::string & target() const;
-
-public: int power() const;
-  
+public: void print(std::ostream &out) const;
 };
+typedef std::shared_ptr < CommaDelimiterNode > CommaDelimeterNodePtr;
 
-class StopCommandNode : public Node {
-public: StopCommandNode(std::shared_ptr< Node > word);
+class CoordinateNode : public Node {
+public: CoordinateNode(const NodePtr &number1, const NodePtr &comma1, const NodePtr &number2, const NodePtr &comma2, const NodePtr &number2);
 public: int type() const;
+public: void print(std::ostream &out) const;
+}
+typedef std::shared_ptr < CoordinateNode > CoordinateNodePtr;
 
-public: const std::string & target() const;
+class PointNode : public Node {
+public: PointNode(const NodePtr &coordinate);
+public: int type() const;
+public: void print(std::ostream &out) const;
+}
+typedef std::shared_ptr < PointNode > PointNodePtr;
 
-};
+class DescriptionNode : public Node {
+public: DescriptionNode(const NodePtr &stringLiteral);
+public: int type() const;
+public: void print(std::ostream &out) const;
+}
+typedef std::shared_ptr < DescriptionNode > DescriptionNodePtr;
+
+class NameNode : public Node {
+public: NameNode(const NodePtr &stringLiteral);
+public: int type() const;
+public: void print(std::ostream &out) const;
+}
+typedef std::shared_ptr < NameNode > NameNodePtr;
+
+class PlacemarkerNode : public Node {
+public: PlacemarkerNode(const NodePtr &name, const NodePtr &description, const NodePtr &point);
+public: int type() const;
+public: void print(std::ostream & out) const;
+}
+typedef std::shared_ptr < PlacemarkerNode > PlacemarkerNodePtr;
+
+class DocumentNode : public Node {
+public: int type() const;
+public: void print(std::ostream &out) const;
+}
+typedef std::shared_ptr < DocumentNode > DocumentNodePtr;
 
 class KMLNode : public Node {
-  public: int type() const;
-};
+public: int type() const;
+public: void print(std::ostream &out) const;
+}
+typedef std::shared_ptr < KMLNode > KMLNodePtr;
 
-
-typedef std::shared_ptr < Node > NodePtr;
+class ProgramNode : public Node {
+public: int type() const;
+public: void print(std::ostream &out) const;
+}
 typedef std::shared_ptr < ProgramNode > ProgramNodePtr;
-typedef std::shared_ptr < WordNode > WordNodePtr;
-typedef std::shared_ptr < NumberNode > NumberNodePtr;
-typedef std::shared_ptr < StartCommandNode > StartCommandNodePtr;
-typedef std::shared_ptr < StopCommandNode > StopCommandNodePtr;
 
-int number(const NodePtr p);
+int numberLiteral(const NodePtr &p);
 
-const std::string & word(const NodePtr p);
+const std::string & stringLiteral(const NodePtr &p);
 
-NodePtr node(int number);
+NodePtr node(int numberLiteral);
 
-NodePtr node(const std::string &word);
+NodePtr node(const std::string &stringLiteral);
+
+std::ostream &operator<< (std::ostream& out, const NodePtr &p);
