fd98e8873fea12e5cf2685d909b9d6af1e59c94c
Charles Bisbee <bisbeecharlesl@gmail.com>
2017-10-09 12:38:03 -0600

    Adding the generator.hpp file

---
+++ Assignment2KML/SourceFiles/generator.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "ast.hpp"
+#include <memory>
+
+struct Generator {
+  NodePtr ast;
+  Generator(const NodePtr &_ast);
+  virtual void generate(std::ostream &out) = 0;
+};
+
+typedef std::shared_ptr <Generator> GeneratorPtr;
+
+GeneratorPtr generator(const NodePtr &ast);
